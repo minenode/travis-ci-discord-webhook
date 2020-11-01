59f4ca6115ef6c4b2653d154b9492a3d0be540c5
Shino <shinotheshino@gmail.com>
2020-11-01 18:57:25 -0500

    Remove extra files

--- send.sh
+++ send.sh
@@ -86,6 +86,6 @@ WEBHOOK_DATA='{
 for ARG in "$@"; do
   echo -e "[Webhook]: Sending webhook to Discord...\\n";
 
-  (curl --fail --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "${WEBHOOK_DATA//	/ }" "$ARG" \
+  (curl --fail --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -d "${WEBHOOK_DATA//	/ }" "$ARG" \
   && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
 done
